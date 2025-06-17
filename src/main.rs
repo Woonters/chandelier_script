4116b2567ad6f0801d3f65dc625815af494047d8
Woonters <woonters@gmail.com>
2025-06-17 21:25:28 +0100

    wasn't planning on gitting this, so this is a mess :/

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+mod parser;
+use chrono::DateTime;
+use chrono::TimeDelta;
+use chrono::TimeZone;
+// some function for reading the csv:
+
+// some function for turning that into instructions
+
+// some function for writing to a new one!
+
+#[derive(Clone, Debug)]
+struct ScheduleElement<Tz: TimeZone> {
+    timestamp: DateTime<Tz>,
+    height: isize,
+}
+
+fn generate_points<Tz: TimeZone>(items: &[ScheduleElement<Tz>]) -> Vec<ScheduleElement<Tz>> {
+    // we take the first item (it's our starting position)
+    let mut out = vec![items[0].clone()];
+    let mut index = 1;
+    loop {
+        // is the next element a different height?
+        if items[index].height != out.last().unwrap().height {
+            // take the difference calculate the number of mins
+            let diff_delta = (items[index].height - out.last().unwrap().height) * 30;
+            let delta = TimeDelta::new(diff_delta as i64, 0)
+                .expect("The difference in height is outside of reasonable range, check the csv");
+            // for some reason although checked_sub and checked_add don't mutate DateTime they both take ownership
+            // of self, so I have to do so much more cloning than one would think proper
+            let ts = items[index].timestamp.clone();
+            let ts2 = ts.clone();
+            let pre = ts.checked_sub_signed(delta).expect("Time is outside of UTC availabilty, are you sure you have set the correct time in the csv");
+            let post = ts2.checked_add_signed(delta).expect("Time is outside of UTC availabilty, are you sure you have set the correct time in the csv");
+            out.push(ScheduleElement {
+                timestamp: pre,
+                height: out.last().unwrap().height,
+            });
+            out.push(ScheduleElement {
+                timestamp: post,
+                height: items[index].height,
+            });
+            index += 1;
+        } else {
+            index += 1;
+        }
+        if index > (items.len() - 1) {
+            return out;
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/parser.rs
@@ -0,0 +1,87 @@
+use core::panic;
+use std::path::Path;
+
+use chrono::{DateTime, Utc};
+#[doc = r"Parsing the input CSV"]
+use winnow::Result;
+use winnow::{
+    Parser,
+    ascii::digit1,
+    combinator::{separated, separated_pair},
+    token::literal,
+};
+
+use crate::ScheduleElement;
+
+pub fn parse_csv(filename: &Path) -> Vec<ScheduleElement<Utc>> {
+    let csv_file = std::fs::read_to_string(filename)
+        .expect("Error getting csv file have you given the correct path?");
+    match _parse_csv(&mut &csv_file[..]) {
+        Ok(v) => v,
+        Err(_) => panic!("Error reading File, is the csv formatted correctly"),
+    }
+}
+
+fn _parse_csv(input: &mut &str) -> Result<Vec<ScheduleElement<Utc>>> {
+    separated(0.., csv_line, literal("\n")).parse_next(input)
+}
+
+fn csv_line(input: &mut &str) -> Result<ScheduleElement<Utc>> {
+    separated_pair(time_parser, literal(", "), parse_number)
+        .map(|(timestamp, height)| ScheduleElement { timestamp, height })
+        .parse_next(dbg!(input))
+}
+
+fn parse_number(input: &mut &str) -> Result<isize> {
+    digit1.parse_to().parse_next(input)
+}
+fn time_parser(input: &mut &str) -> Result<DateTime<Utc>> {
+    input.parse_to::<DateTime<Utc>>().parse_next(input)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use chrono::{Datelike, Timelike};
+
+    use super::{csv_line, *};
+
+    #[test]
+    fn total() {
+        let parsed = parse_csv(Path::new("test_csv.csv"));
+        assert_eq!(
+            parsed.len(),
+            4,
+            "csv wasn't parsed into the correct number of lines"
+        );
+    }
+
+    #[test]
+    fn timestamp() {
+        let mut time = "2025-01-01T01:00:00Z";
+        let time_parsed = time_parser(&mut time);
+        assert!(time_parsed.is_ok());
+        assert_eq!(time_parsed.unwrap().year(), 2025);
+        assert_eq!(time, "");
+    }
+
+    #[test]
+    fn number() {
+        let mut num = "15";
+        let num_parsed = parse_number(&mut num);
+        assert!(num_parsed.is_ok());
+        assert_eq!(num_parsed.unwrap(), 15, "Numbers were not the same");
+        assert_eq!(num, "");
+    }
+
+    #[test]
+    fn test_csv_line() {
+        let mut line = "2025-01-01T01:00:00Z, 15";
+        let line_parsed = csv_line(&mut line);
+        assert!(line_parsed.is_ok());
+        let parsed = line_parsed.unwrap();
+        assert_eq!(parsed.height, 15);
+        assert_eq!(parsed.timestamp.hour(), 1);
+        assert_eq!(line, "");
+    }
+}
